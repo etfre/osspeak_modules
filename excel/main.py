ab950f9939989d7688ee47cffcde18cd50ecfb07
evfredericksen <evfredericksen@gmail.com>
2017-01-13 23:29:22 -0500

    test

---
+++ excel/main.py
@@ -0,0 +1,10 @@
+import sprecgrammars
+import time
+
+def main():
+    print('excel extension activated')
+    msg = osspeak_queue.get(block=True)
+    print('got message: ', msg)
+    print ('shutting down...')
+
+main()
