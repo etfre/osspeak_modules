e11332391974f43ae1a9fa9f144695057311cc35
Evan <evfredericksen@gmail.com>
2017-02-03 23:56:18 -0500

    terminal nav

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,21 +1,23 @@
-from platforms.api import transcribe_line
+from platforms.api import type_line
+import time
 import os
 import tempfile
 
-def drop(self, num):
+def drop():
     num = osspeak_queue.get(block=True)
     num = int(num)
     _, temp_name = tempfile.mkstemp()
-    results =transcribe_line('ls > {}'.format(temp_name) + '{enter}')
+    results = type_line([f'ls > {temp_name}', ['enter']])
     time.sleep(.5)
     try:
         with open(temp_name) as f:
             for i, line in enumerate(f, start=1):
                 if i == num:
-                   transcribe_line('cd {}'.format(line.rstrip('\n')) + '{enter}')
+                    line = line.rstrip("\n")
+                    type_line([f'cd {line}', ['enter']])
                     break
     finally:
         if os.path.isfile(temp_name):
-    os.remove(temp_name)
+            os.remove(temp_name)
 
 drop()
